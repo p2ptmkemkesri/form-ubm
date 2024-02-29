7796bc6303b8290b1bb22c12e087e381a9dfb3fe
p2ptmkemkesri <websitep2ptm2023>
2024-02-29 15:12:14 +0700

    all province

--- form-ubm/database/migrations/2023_12_20_031936_create_provinsis_table.php
+++ form-ubm/database/migrations/2023_12_20_031936_create_provinsis_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('provinsis', function (Blueprint $table) {
             $table->id();
             $table->string('nama_provinsi')->nullable();
+            $table->string('kode_provinsi')->nullable();
             $table->timestamps();
         });
     }
