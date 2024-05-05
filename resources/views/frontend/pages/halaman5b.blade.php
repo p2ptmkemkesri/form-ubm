7afffe10370bd7b32d51410c2a7b4522519dd12d
p2ptmkemkesri <websitep2ptm2023>
2024-05-05 19:10:39 +0700

    soal selesai ditulis

--- resources/views/frontend/pages/halaman5b.blade.php
+++ resources/views/frontend/pages/halaman5b.blade.php
@@ -19,7 +19,7 @@
     {{-- pertanyaan E3 --}}
     <p class="pertanyaan mt-4" id="e3">
     <div class="form-group">
-        <label for="e3" class="label">D4. Apakah teman-teman dekatmu banyak yang merokok?
+        <label for="e3" class="label">E3. Apakah teman-teman dekatmu banyak yang merokok?
         </label><br>
 
         <input type="radio" name="e3" value="Ya" id="e3" selected> Ya
